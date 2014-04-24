104385fe973739237f6ae4a563ee3b182078fba6
Gregory Favre <gregory.favre@gmail.com>
2014-04-24 18:23:59 +0200

    admin for domains

--- beyondthehost/domains/admin.py
+++ beyondthehost/domains/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Domain, SubDomain
 
-# Register your models here.
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'subdomains')
+    list_filter = ('owner',)
+    ordering = ('owner__full_name', 'name')
+    
+    def subdomains(self, obj):
+        return ', '.join([subdomain.name for subdomain in obj.subdomains.all() if subdomain.name])
+    
+
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(SubDomain)
