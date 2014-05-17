1d60569129964da7e2cc579b8510e84e487b7ec1
Gregory Favre <gregory.favre@gmail.com>
2014-05-17 10:25:53 +0200

    added list websites feature

--- beyondthehost/websites/urls.py
+++ beyondthehost/websites/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, include, url
-from django.core.files.storage import FileSystemStorage
+from django.conf.urls import patterns, url
 
-from .views import WebsiteWizard
+from .views import WebsiteWizard, ListWebsitesView
 
 wizard = WebsiteWizard.as_view(url_name='website-wizard_step', done_step_name='finished')
-urlpatterns = patterns('',
-    url(r'^wizard/(?P<step>.+)/$', wizard, name='website-wizard_step'),
-    url(r'^wizard/$', wizard, name="website-wizard")
 
+urlpatterns = patterns('',
+    url(r'^$', ListWebsitesView.as_view(), name="website-list"),
+    url(r'^wizard/(?P<step>.+)/$', wizard, name='website-wizard_step'),
+    url(r'^wizard/$', wizard, name="website-wizard"),
 )
 
--- beyondthehost/websites/views.py
+++ beyondthehost/websites/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard.views import NamedUrlSessionWizardView
+from django.views.generic import ListView
 
+from braces.views import LoginRequiredMixin
 
+from beyondthehost.models import OwnedMixin
 from applications.forms import AppForm
 from domains.forms import DomainSelectForm
+
 from .forms import URLForm
+from .models import Website
 
 def show_create_domain(wizard):
     cleaned_data = wizard.get_cleaned_data_for_step('paytype') or {'method': 'none'}
 
 
 class WebsiteWizard(NamedUrlSessionWizardView):
-    template_name = 'website/wizard/wizard_form.html'
+    template_name = 'websites/wizard/wizard_form.html'
     form_list = [('app', AppForm), ('url', URLForm), ('domain', DomainSelectForm)]
     condition_dict = {}
     
@@ -27,5 +32,8 @@ class WebsiteWizard(NamedUrlSessionWizardView):
         return kwargs
 
 
-
-# Create your views here.
+class ListWebsitesView(LoginRequiredMixin, OwnedMixin, ListView):
+    model = Website
+    
+    def get_queryset(self):
+        return self.model.objects.prefetch_related('domain')
